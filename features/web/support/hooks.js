f4afd3a87e69f1082ad067f13b0831dc4ea104c7
Angel Henao <ay.henao@uniandes.edu.co>
2024-11-10 18:10:08 -0500

    Se instala paginas en los hooks de Kraken

--- features/web/support/hooks.js
+++ features/web/support/hooks.js
@@ -3,6 +3,7 @@ const { WebClient } = require("kraken-node");
 const Login = require("../../../page-objects/login.model");
 const NavBar = require("../../../page-objects/nav-bar.model");
 const Members = require("../../../page-objects/members.model");
+const Pages = require("../../../page-objects/pages.model");
 
 Before(async function () {
   this.deviceClient = new WebClient("chrome", {}, this.userId);
@@ -10,6 +11,7 @@ Before(async function () {
   this.login = new Login(this.driver);
   this.navBar = new NavBar(this.driver);
   this.members = new Members(this.driver);
+  this.pages = new Pages(this.driver);
 });
 
 After(async function () {
